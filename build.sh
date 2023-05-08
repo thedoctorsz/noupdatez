17a003d7f2da860c563c4e5de96e218547ca4ab0
noupdate <dedoctorsz@gmail.com>
2023-05-08 23:38:09 +0000

    noupdatez: import scripts [skip ci]

---
+++ build.sh
@@ -0,0 +1,23 @@
+# sync rom
+repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 13 --git-lfs -g default,-mips,-darwin,-notdefault
+git clone https://github.com/thedoctorsz/local_manifest --depth 1 -b banana-13 .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
+# build rom
+source $CIRRUS_WORKING_DIR/config
+timeStart
+
+source build/envsetup.sh
+export BUILD_USERNAME="$USERNAME"
+export BUILD_HOSTNAME="$HOSTNAME"
+lunch banana_whyred-userdebug
+mkfifo reading # Jangan di Hapus
+tee "${BUILDLOG}" < reading & # Jangan di Hapus
+build_message "Building Started" # Jangan di Hapus
+progress & # Jangan di Hapus
+m banana -j8 > reading #& sleep 95m # Jangan di hapus text line (> reading)
+
+retVal=$?
+timeEnd
+statusBuild
+# build with gapps
